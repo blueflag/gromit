7abef77f5b94600addfc886b34b82a1a6f914947
Dan Coates <dan.coates@blueflag.com.au>
2017-07-19 17:51:30 +1000

    Fix flow and update api slightly

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
 //@flow
 import axios from 'axios';
 
-type GromitBodyData = string | Object | ArrayBuffer | ArrayBufferView | URLSearchParams | FormData | File | Blob | Stream | Buffer;
+type GromitBodyData = string | Object | ArrayBuffer | $ArrayBufferView | URLSearchParams | FormData | File | Blob | stream$Readable | Buffer;
 type GromitParamData = Object | URLSearchParams;
 type GromitMethod = 'get' | 'post' | 'put' | 'patch' | 'delete' | 'head' | 'options';
 
 type GromitConfiguration = {
-    method: GromitMethod,
-    path: string,
-    data: GromitBodyData,
-    params: GromitParamData,
-    headers: Object,
-    baseUrl: string,
-    timeout: number,
-    withCredentials: bool,
-    auth: {
+    method?: GromitMethod,
+    path?: string,
+    data?: GromitBodyData,
+    params?: GromitParamData,
+    headers?: Object,
+    baseUrl?: string,
+    timeout?: number,
+    withCredentials?: bool,
+    auth?: {
         username: string,
         password: string
     },
-    responseType: 'arraybuffer' | 'blob' | 'document' | 'json' | 'text' | 'stream',
-    maxContentLength: number,
-    maxRedirects: number
+    responseType?: 'arraybuffer' | 'blob' | 'document' | 'json' | 'text' | 'stream',
+    maxContentLength?: number,
+    maxRedirects?: number,
+    autoFetch?: bool,
+    onUploadProgress?: (progressEvent: ProgressEvent) => void,
+    onDownloadProgress?: (progressEvent: ProgressEvent) => void
 };
 
 
 const defaultConfig = {
-    method: 'get'
+    method: 'get',
+    autoFetch: true
 };
 
 type GromitSuccessResponseData = {
-    statusCode: number;
-    statusText: string;
-    data: any;
-    headers: Object;
-    request: XMLHttpRequest | ClientRequest;
+    statusCode: number,
+    statusText: string,
+    data: any,
+    headers: Object,
+    request: XMLHttpRequest | http$ClientRequest
 };
 
 export class GromitSuccess {
@@ -42,7 +46,7 @@ export class GromitSuccess {
     statusText: string;
     data: any;
     headers: Object;
-    request: XMLHttpRequest | ClientRequest;
+    request: XMLHttpRequest | http$ClientRequest;
     constructor(
         responseData: GromitSuccessResponseData,
         requester: Gromit
@@ -55,7 +59,7 @@ export class GromitSuccess {
         this.statusText = responseData.statusText;
     }
 
-    static fromAxiosResponse(response: Object, requester: Gromit) {
+    static fromAxiosResponse(response: Object, requester: Gromit): GromitSuccess {
         return new GromitSuccess({
             request: response.request,
             data: response.data,
@@ -73,6 +77,7 @@ type GromitErrorResponseData = {
 
 export class GromitError extends Error {
     requester: Gromit;
+    statusCode: number;
     constructor(
         error: Error,
         errorData: GromitErrorResponseData,
@@ -83,7 +88,7 @@ export class GromitError extends Error {
         this.statusCode = errorData.statusCode;
     }
 
-    static fromAxiosError(error: Error, requester: Gromit) {
+    static fromAxiosError(error: Object, requester: Gromit): GromitError {
         return new GromitError(error, {
             statusCode: error.response.status
         }, requester);
@@ -105,66 +110,66 @@ class Gromit {
         return new Gromit(configurator(this.configuration));
     }
 
+    uploadProgress(cb: (progressEvent: ProgressEvent) => void): Gromit {
+        return new Gromit({
+            ...this.configuration,
+            onUploadProgress: cb
+        });
+    }
+
+    downloadProgress(cb: (progressEvent: ProgressEvent) => void): Gromit {
+        return new Gromit({
+            ...this.configuration,
+            onDownloadProgress: cb
+        });
+    }
+
+
     request(
-        method: GromitMethod,
-        path: string,
-        data: GromitBodyData,
-        params: GromitParamData
-    ): Gromit {
+        method: ?GromitMethod,
+        path: ?string,
+        data: ?GromitBodyData,
+        params: ?GromitParamData
+    ): Promise<GromitSuccess> {
         return new Gromit({
             ...this.configuration,
             method: method || this.configuration.method,
             path: typeof path === 'string' ? path : this.configuration.path,
             data: data || this.configuration.data,
             params: params || this.configuration.params
-        });
+        }).fetch();
     }
 
-    get(path: string, params: GromitParamData): Gromit {
+    get(path?: string, params?: GromitParamData): Promise<GromitSuccess> {
         return this.request('get', path, null, params);
     }
 
-    post(path: string, data: GromitBodyData, params: GromitParamData): Gromit {
+    post(path?: string, data?: GromitBodyData, params?: GromitParamData): Promise<GromitSuccess> {
         return this.request('post', path, data, params);
     }
 
-    put(path: string, data: GromitBodyData, params: GromitParamData): Gromit {
+    put(path?: string, data?: GromitBodyData, params?: GromitParamData): Promise<GromitSuccess> {
         return this.request('put', path, data, params);
     }
 
-    patch(path: string, data: GromitBodyData, params: GromitParamData): Gromit {
+    patch(path?: string, data?: GromitBodyData, params?: GromitParamData): Promise<GromitSuccess> {
         return this.request('patch', path, data, params);
     }
 
-    delete(path: string, params: GromitParamData): Gromit {
+    delete(path?: string, params?: GromitParamData): Promise<GromitSuccess> {
         return this.request('delete', path, null, params);
     }
 
-    head(path: string, params: GromitParamData): Gromit {
+    head(path?: string, params?: GromitParamData): Promise<GromitSuccess> {
         return this.request('head', path, null, params);
     }
 
-    options(path: string, params: GromitParamData): Gromit {
+    options(path?: string, params?: GromitParamData): Promise<GromitSuccess> {
         return this.request('options', path, null, params);
     }
 
 
-    uploadProgress(cb: (progressEvent: ProgressEvent) => void) {
-        return new Gromit({
-            ...this.configuration,
-            onUploadProgress: cb
-        }, this.response);
-    }
-
-    downloadProgress(cb: (progressEvent: ProgressEvent) => void) {
-        return new Gromit({
-            ...this.configuration,
-            onDownloadProgress: cb
-        }, this.response);
-    }
-
-
-    fetch() {
+    fetch(): Promise<GromitSuccess> {
         return axios({
             method: this.configuration.method,
             baseURL: this.configuration.baseUrl,
@@ -182,10 +187,9 @@ class Gromit {
             onDownloadProgress: this.configuration.onDownloadProgress
         })
             .then(response => GromitSuccess.fromAxiosResponse(response, this))
-            .catch(error => Promise.reject(GromitError.fromAxiosError(error, error, this)));
+            .catch(error => Promise.reject(GromitError.fromAxiosError(error, this)));
 
     }
-
 
 }
 
@@ -193,3 +197,13 @@ class Gromit {
 export default function GromitFactory(configuration: GromitConfiguration): Gromit {
     return new Gromit(configuration);
 }
+
+
+export const graphqlRequest = (url: string, getToken: () => Promise<string>) => async (query: string, variables: Object): Promise<Object> => {
+    const token = await getToken();
+    return GromitFactory({headers: {
+        'Authorization': token
+    }})
+        .post(url, {query, variables})
+        .then(response => response.data);
+};
